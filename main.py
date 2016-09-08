3bcb7f3cb881cab5ca217e95a0d36b4debb2aa9c
Koen Wolters <koen@bluejayeindhoven.nl>
2016-09-08 19:37:47 +0200

    make module

--- main.py
+++ main.py
@@ -4,22 +4,25 @@ os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = '1'
 import numpy as np
 import matplotlib.pyplot as plt
 
-from read import read_pos
-from prepare import bin_data, limit_bins, zero_extend
-from algs import estimate_baseline, find_peaks
+from .read import read_pos
+from .prepare import bin_data, limit_bins, zero_extend
+from .algs import estimate_baseline, find_peaks
 
 from scipy.signal import gaussian, savgol_filter, convolve
 
 import sortedcontainers
 
-def prepare():
+def read(file):
     print("Reading data...");
-    data = read_pos('R62_01328-v07.pos', 200000000)
+    data = read_pos(file)
     
     print("Binning data...");
     bins, width = bin_data(data['m'], 0.01)
     bins = zero_extend(bins, width)
     
+    return bins, width
+
+def prepare(bins, width):    
     print("Baseline removal...")
     baseline = estimate_baseline(bins, width, window=25, max_baseline=100)
     bins['height'] = bins['height']-baseline
@@ -29,20 +32,22 @@ def prepare():
     gauss /= sum(gauss)
     bins['height'] = np.convolve(bins['height'], gauss, mode='same')
     
-    return bins
-
-def run(bins):
-    plt.clf()
     #bins['height'] = savgol_filter(bins['height'], 101, 7)
     
+    return bins
+
+def run(bins, width):
     print("Finding peaks...");
     peaks = find_peaks(bins, min_peak_size=5, peak_diff=0.5)
     print(peaks)
     
-    plt.plot(bins['edge']+width/2, bins['height'])
-    bins['height'] = bins['height']+baseline
-    plt.plot(bins['edge']+width/2, bins['height'], color='r')
-    #plt.plot(bins['edge']+width/2, baseline)
+    return peaks
+
+def plot(bins, raw_bins, peaks, width):
+    plt.clf()
+    plt.plot(raw_bins['edge']+width/2, raw_bins['height'], color='r')
+    plt.plot(bins['edge']+width/2, bins['height'], color='b')
+    
     for el in peaks:
         plt.axvline(el['edge']+width/2, color='g')
     plt.yscale('log')
@@ -52,8 +57,14 @@ def run(bins):
     axes = plt.gca()
     axes.set_ylim(ymin = 1)
     plt.show()
-
     
 if __name__ == "__main__":
-    bins = prepare()
-    run(bins)
+    file = "R62_01328-v07.pos"
+
+    raw_bins, width = read(file)
+    bins = prepare(raw_bins.copy(), width)
+    
+    peaks = run(bins, width)
+    plot(bins, raw_bins, peaks, width)
+    
+    
